34c18e3679ab154389c76225cdb41692cd1631d9
bezachka <ilyaershov135@gmail.com>
2024-05-10 11:27:55 +0300

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,20 @@
+def f(n):
+    ost = ''
+    while n != 0:
+        ost = str(n % 3) + ost
+        n = n // 3
+
+    return ost[::-1]
+sp = []
+for n in range(1,200):
+    n3 = f(n)
+    if n % 2 == 0:
+        n3 = '2' + n3 + f(n3[-1] * 2)
+    else:
+        n3 = f(n3[0] * 2) + n3 + '2'
+
+    r = int(n3, 3)
+    if r > 100:
+        sp.append(r)
+
+print(min(sp))
